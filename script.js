ff70986c0263c9e7427582181626bec913146323
Shubhangi Bhapkar <shubhangibhapkar35@gmail.com>
2026-08-17 11:59:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,27 +11,56 @@ const products = [
 
 // DOM elements
 const productList = document.getElementById("product-list");
+let cart = JSON.parse(sessionStorage.getItem("cart")) || [];
 
 // Render product list
 function renderProducts() {
+  
   products.forEach((product) => {
     const li = document.createElement("li");
-    li.innerHTML = `${product.name} - $${product.price} <button class="add-to-cart-btn" data-id="${product.id}">Add to Cart</button>`;
+    li.innerHTML = `${product.name} - $${product.price} <button class="add-to-cart-btn" onclick="addToCart(${product.id})" data-id="${product.id}">Add to Cart</button>`;
     productList.appendChild(li);
   });
 }
 
 // Render cart list
-function renderCart() {}
+function renderCart() {
+	// const showCart = document.createElement("div");
+	// showcart.innerHTML = sessionStorage.getItem()
+	
+}
 
 // Add item to cart
-function addToCart(productId) {}
+function addToCart(productId) {
+    console.log("product added in cart", productId);
 
+    const selectedProduct = products.find(
+        product => product.id == productId
+    );
+
+    cart.push(selectedProduct);
+    console.log(cart);
+    window.sessionStorage.setItem("cart", JSON.stringify(cart));
+
+    console.log(selectedProduct.name);
+    console.log(selectedProduct.price);
+
+    let prodObj = {
+        Name: selectedProduct.name,
+        Price: selectedProduct.price
+    };
+    sessionStorage.setItem("cart", JSON.stringify(prodObj));
+}
 // Remove item from cart
-function removeFromCart(productId) {}
+function removeFromCart(productId) {
+  console.log("removing item from cart..");
+	sessionStorage.removeItem("cart");
+}
 
 // Clear cart
-function clearCart() {}
+function clearCart() {
+	sessionStorage.clear();
+}
 
 // Initial render
 renderProducts();
